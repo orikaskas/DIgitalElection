4befd8f845e897e3fb2c6addf38da03d9841faf3
Student18 <orikaskas1234@gmail.com>
2024-03-14 10:40:33 +0200

    a

--- app/src/main/java/com/example/digitalelections/UI/HomePackage/HomePage.java
+++ app/src/main/java/com/example/digitalelections/UI/HomePackage/HomePage.java
@@ -2,6 +2,7 @@ package com.example.digitalelections.UI.HomePackage;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,10 +10,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.example.digitalelections.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomePage extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
 
 
     @Override
@@ -20,7 +27,31 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_home_page);
+        bottomNavigationView = findViewById(R.id.BottomNavigationBar);
+        fragmentManager = getSupportFragmentManager();
 
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.home){
+                    fragmentManager.beginTransaction().replace(R.id.frameLayout,HomeFragment.class,null).commit();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.profile) {
+                    fragmentManager.beginTransaction().replace(R.id.frameLayout, profileFragment.class,null).commit();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.country) {
+                    fragmentManager.beginTransaction().replace(R.id.frameLayout,CountryFragment.class,null).commit();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.city) {
+                    fragmentManager.beginTransaction().replace(R.id.frameLayout, CityFragment.class,null).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 }
