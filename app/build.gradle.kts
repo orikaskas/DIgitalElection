44e9e5ac3db61b77fd490a728f1e39675a08f58d
orikaskas <Orikaskas1234@gmail.com>
2024-06-06 00:47:14 +0300

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,13 +43,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.1")
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("androidx.activity:activity:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-storage")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("com.squareup.picasso:picasso:2.71828")
-
-
 }
